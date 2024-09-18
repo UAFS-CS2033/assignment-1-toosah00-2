3298764acf057791d7782930c58c6793aa7291ee
Thaden Oosahwe <toosah00@uafs.edu>
2024-09-17 23:03:22 -0500

    update

--- Server.java
+++ Server.java
@@ -7,9 +7,7 @@ import java.net.Socket;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileInputStream;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+
 
 
 public class Server{
@@ -22,19 +20,20 @@ public class Server{
     }
 
     void requestFullfillment(String filePath , String contentType , PrintWriter out) throws IOException{
+        File file = new File(filePath);
+        int contentLength = (int) file.length();
+
+        out.printf("HTTP/1.1 200 OK\n");
+        out.printf("Content-Length: %d\n", contentLength);
+        out.printf("Content-Type: %s\n\n", contentType);
+
+        System.out.printf("HTTP/1.1 200 OK\n");
+        System.out.printf("Content-Length: %d\n", contentLength);
+        System.out.printf("Content-Type: %s\n\n", contentType);
+
+
 
         if(!(contentType.equals("image/png"))){
-            File file = new File(filePath);
-            int contentLength = (int) file.length();
-
-            out.printf("HTTP/1.1 200 OK\n");
-            System.out.printf("HTTP/1.1 200 OK\n");
-            out.printf("Content-Length: %d\n", contentLength);
-            System.out.printf("Content-Length: %d\n", contentLength);
-            out.printf("Content-Type: %s\n\n", contentType);
-            System.out.printf("Content-Type: %s\n\n", contentType);
-
-
             BufferedReader br = new BufferedReader(new FileReader (file));
             String buffer = br.readLine();
             while(buffer != null){
@@ -44,44 +43,24 @@ public class Server{
             } 
             br.close();
         } else {
-            File imageFile = new File(filePath);
-            FileInputStream imageStream = new FileInputStream(imageFile);
-            int contentLength = (int) imageFile.length();
-            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("foo.out")));
-
-            out.printf("HTTP/1.1 200 OK\n");
-            System.out.printf("HTTP/1.1 200 OK\n");
-            out.printf("Content-Length: %d\n", contentLength);
-            System.out.printf("Content-Length: %d\n", contentLength);
-            out.printf("Content-Type: %s\n\n", contentType);
-            System.out.printf("Content-Type: %s\n\n", contentType);
-
-            pw.print(imageStream);
-
-            pw.close();
-
+            // ?
             
-
         }
-
-        
-        
     }
 
     private void processConnection() throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
     
-        /*//*** Application Protocol *****
+        //*** Application Protocol *****
         String buffer = in.readLine();
-        while(buffer.length() != 0){
-            System.out.println(buffer);
-            buffer = in.readLine();
-        } */
-
-        requestFullfillment("docroot/home.html" , "text/html", out);
-        requestFullfillment("docroot/scripts/style.css" , "text/css", out);
-        requestFullfillment("docroot/images/assign2-screen.png" , "image/png" , out );
+        if(buffer.equals("GET /home.html HTTP/1.1")){
+            requestFullfillment("docroot/home.html" , "text/html", out);
+        } else if(buffer.equals("GET /scripts/style.css HTTP/1.1")){
+            requestFullfillment("docroot/scripts/style.css" , "text/css", out);
+        } else if (buffer.equals("GET /images/assign2-screen.png HTTP/1.1")){
+            //requestFullfillment("docroot/images/assign2-screen.png" , "image/png" , out );
+        }    
         
         in.close();
         out.close();
